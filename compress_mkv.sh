cd9c266377789b5186bf87e7e8d7d1517670b8c5
Jack O'Connor <jack@linkit.com>
2026-04-16 12:42:17 -0400

    change /bin/bash to /usr/bin/env bash for portability

--- compress_mkv.sh
+++ compress_mkv.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Perform lossless H264 compression on a ripped MKV file
 # Positional Arguments:
--- rip_disk.sh
+++ rip_disk.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 MOVIE_ROOT="/Volumes/Sandisk/Raw Movies"
 

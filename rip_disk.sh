70a4ffd3f8f1215a1bc9d9bc866b121994e81346
Jack O'Connor <jackeamon@icloud.com>
2026-04-18 00:37:08 -0400

    Create orchestrate.sh and modify dependent scripts accordingly
    orchestrate.sh allows rip_disk.sh, organize_files.sh, compress_mkv.sh,
    and sync-movie-to-server.sh to be run sequentially without the need for
    manual intervention. The only required argument is the IMDB ID of the
    title.

--- rip_disk.sh
+++ rip_disk.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-MOVIE_ROOT="/Volumes/Sandisk/Raw Movies"
+: ${RAW_MOVIES_BASE:="/Volumes/Sandisk/Raw Movies"}
 
-MESSAGES_PATH="$MOVIE_ROOT/messages.txt"
+MESSAGES_PATH="$RAW_MOVIES_BASE/messages.txt"
 
 makemkvcon info -r disc:9999 --messages="$MESSAGES_PATH"
 
@@ -10,15 +10,17 @@ IFS=, read -r raw_drive_name _ _ _ _ disc_name _ < <(grep -m1 '^DRV:' "$MESSAGES
 
 drive_name="disc:${raw_drive_name##*:}"
 
-if [[ ! -z $disc_name ]]; then
-  echo $disc_name
-else
+if [[ -z $disc_name ]]; then
   echo "Error: Disc name is empty" >&2
   exit 1
 fi
 
-OUTPUT="$MOVIE_ROOT/${disc_name//\"/}"
+OUTPUT="$RAW_MOVIES_BASE/${disc_name//\"/}"
 
 mkdir -p "$OUTPUT"
 
 makemkvcon mkv -r --cache=1024 "$drive_name" all "$OUTPUT" --messages="$OUTPUT/log.txt" --progress=-same
+
+printf 'MAKEMKV_FOLDER_NAME=%q\n' "$(echo "$disc_name" | tr -d '"')"
+
+exit 0

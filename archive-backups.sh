07269c3e8aaa96518601eb49a62f206286f8a420
Jack O'Connor <jack@linkit.com>
2026-04-28 13:18:17 -0400

    partial progress in case of pipefail or interrupt

--- archive-backups.sh
+++ archive-backups.sh
@@ -22,14 +22,14 @@ if [[ ! $INPUT =~ ^/ ]]; then
   exit 1
 fi
 
-if [[ ! $INPUT =~ /$ ]]; then
-  echo "Input must end with a slash; this program is designed to copy only the contents of the Raw Movies directory" >&2
-  exit 1
-fi
+# if [[ ! $INPUT =~ /$ ]]; then
+#   echo "Input must end with a slash; this program is designed to copy only the contents of the Raw Movies directory" >&2
+#   exit 1
+# fi
 
 REMOTE_DEST="jackoconnor@192.168.50.2:'/mnt/storage/raw-movies-backup/'"
 
-rsync -rltDvhW --delete --partial ${DRY_RUN_ARG:+$DRY_RUN_ARG} --progress --stats --exclude '.DS_Store' \
+rsync -rltDvh --delete --partial ${DRY_RUN_ARG:+$DRY_RUN_ARG} --append --progress --stats --exclude '.DS_Store' \
   -e "ssh -T -c aes128-gcm@openssh.com -o Compression=no -o ServerAliveInterval=30 -o ServerAliveCountMax=6" \
   --rsync-path='sudo -n rsync' \
   --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r \

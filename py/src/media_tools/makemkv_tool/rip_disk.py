e401fb26c0d579247248694d5ba87efabcf1f81f
Jack O'Connor <jack@linkit.com>
2026-05-06 00:07:26 -0400

    Add makemkvcon mkv step to rip_disk

--- py/src/media_tools/makemkv_tool/rip_disk.py
+++ py/src/media_tools/makemkv_tool/rip_disk.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from media_tools.makemkv_tool.progress_tracker import MakeMKVProgressTracker
+
 from .client import MakeMKVClient
 from .info_parser import MKVInfoParser
 
@@ -22,14 +24,17 @@ def check_existing_mkvs(output_path):
     return
 
 
-# TODO: option to rip largest title, interactive mode, or all content on disc
-# TODO: dipslay some printed output for each major step and/or display a separate step counter
-def rip_disk(verbose: bool = False, output_base: Path | None = None):
+# TODO: improve title selection so you can see all titles
+# instead of one at a time
+# TODO: make more testable with mock data for the mkv ripping step
+def rip_disk(output_base: Path, verbose: bool = False, overwrite: bool = False):
 
     drive_name = None
     mkv_client = MakeMKVClient()
 
+    # TODO: this can be done better by using `drutil` to save time (however, may be less compatible)
     drive_info_parser = MKVInfoParser()
+    print("Identifying active drives...")
     try:
         for line in mkv_client.get_info_lines():
             drive_info_parser.handle_line(line)
@@ -40,46 +45,45 @@ def rip_disk(verbose: bool = False, output_base: Path | None = None):
 
     assert drive_name is not None
     disc_info_parser = MKVInfoParser()
+    print("Extracting disc information...")
     try:
         for line in mkv_client.get_info_lines(drive_name):
             disc_info_parser.handle_line(line)
     finally:
         disc_info = disc_info_parser.build()
 
-    return disc_info
+    disc_title = disc_info.disc_title
 
     # MakeMKV mkv rip step: track progress
-    # output_path = output_base / disc_name
-    #
-    # cache_size = None
-    # if disc_info.disc_type == "BD":
-    #     cache_size = 1024
-    # elif disc_info.disc_type == "DVD":
-    #     cache_size = 512
-    #
-    # assert cache_size is not None
-    #
-    # check_existing_mkvs(output_path)
-    #
-    # mkv_proc = subprocess.Popen(
-    #     [
-    #         "makemkvcon",
-    #         "mkv",
-    #         "-r",
-    #         f"--cache={cache_size}",
-    #         "--progress=-stdout",
-    #         raw_drive_name,
-    #         "all",
-    #         output_path,
-    #     ],
-    #     stdout=subprocess.PIPE,
-    #     stderr=subprocess.STDOUT,
-    #     text=True,
-    #     bufsize=1,
-    # )
-    #
-    # assert mkv_proc.stdout is not None
-    #
-    # MakeMKVProgressTracker(mkv_proc.stdout).track_progress()
-    #
-    # return mkv_proc.wait()
+    output_path = output_base / disc_title
+
+    cache_size = None
+    if disc_info.disc_type == "BD":
+        cache_size = 1024
+    elif disc_info.disc_type == "DVD":
+        cache_size = 512
+
+    assert cache_size is not None
+
+    if not overwrite:
+        check_existing_mkvs(output_path)
+
+    titles = disc_info.titles.values()
+    titles_to_rip = []
+    for title in titles:
+        response = input(
+            f"{title.title_name} (title_id {title.title_id}) has duration {title.duration}. Would you like to rip it? (y/N): "
+        )
+        if response in ("y", "Y", "Yes", "yes", "YES"):
+            titles_to_rip.append(title.title_id)
+
+    print("Beginning makemkvcon...")
+    for i, title_id in enumerate(titles_to_rip):
+        print(f"Ripping title_id {title_id} ({i}/{len(titles_to_rip)})")
+        mkv_progress = MakeMKVProgressTracker()
+        for line in mkv_client.start_mkv_process(
+            drive_name, output_path, cache_size=cache_size, title_id=title_id
+        ):
+            if verbose:
+                print(line)
+            mkv_progress.handle_line(line)

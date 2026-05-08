afc11ba14b7f9d4a910a4d03803083aa4758f1eb
Jack O'Connor <jack@linkit.com>
2026-05-08 16:23:15 -0400

    Rename progress_tracker to progress

--- py/src/media_tools/makemkv_tool/__init__.py
+++ py/src/media_tools/makemkv_tool/__init__.py
@@ -5,7 +5,7 @@ and rip its contents to a specified output directory
 
 from .client import MakeMKVClient
 from .info_parser import MKVInfoParser
-from .progress_tracker import MakeMKVProgressTracker
+from .progress import MakeMKVProgressTracker
 from .rip_disk import rip_disk
 
 __all__ = ["MakeMKVClient", "MKVInfoParser", "MakeMKVProgressTracker", "rip_disk"]

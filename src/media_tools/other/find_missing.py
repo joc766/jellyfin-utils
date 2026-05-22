fd244563ff9d92a14c414a5ed4e624a61a07041f
Jack O'Connor <jackeamon@icloud.com>
2026-05-22 18:08:46 -0400

    Use imdb_ids for finding missing compressed vs. raw

--- src/media_tools/other/find_missing.py
+++ src/media_tools/other/find_missing.py
@@ -1,6 +1,13 @@
+import re
 from pathlib import Path
 
 import paramiko
+
+
+def get_imdb_id(folder_name: str) -> str | None:
+    imdb_pattern = re.compile(r".*\[imdbid-(tt\d+)\]")
+    if match := imdb_pattern.match(folder_name):
+        return match.group(1)
 
 
 def find_missing_raw_movies(
@@ -10,10 +17,21 @@ def find_missing_raw_movies(
         client.load_system_host_keys()
         client.connect(jellyfin_host, username=jellyfin_user)
         with client.open_sftp() as sftp_client:
-            compressed_dirs = set(sftp_client.listdir(str(jellyfin_base / "compressed" / "movie")))
-            raw_dirs = set(sftp_client.listdir(str(jellyfin_base / "raw" / "movie")))
+            compressed_dirs = {
+                get_imdb_id(str(folder)): folder
+                for folder in sftp_client.listdir(str(jellyfin_base / "compressed" / "movie"))
+                if get_imdb_id(str(folder)) is not None
+            }
+            raw_dirs = {
+                get_imdb_id(str(folder)): folder
+                for folder in sftp_client.listdir(str(jellyfin_base / "raw" / "movie"))
+                if get_imdb_id(str(folder)) is not None
+            }
+    missing_ids = set(compressed_dirs.keys()) - set(raw_dirs.keys())
 
-    return [str(Path(dir).stem) for dir in compressed_dirs - raw_dirs]
+    return [
+        str(Path(dir).name) for imdb_id, dir in compressed_dirs.items() if imdb_id in missing_ids
+    ]
 
 
 def find_missing_compressed_movies(
@@ -23,9 +41,17 @@ def find_missing_compressed_movies(
         client.load_system_host_keys()
         client.connect(jellyfin_host, username=jellyfin_user)
         with client.open_sftp() as sftp_client:
-            compressed_dirs = set(sftp_client.listdir(str(jellyfin_base / "compressed" / "movie")))
-            raw_dirs = set(sftp_client.listdir(str(jellyfin_base / "raw" / "movie")))
-            print(raw_dirs)
-            print(compressed_dirs)
+            compressed_dirs = {
+                get_imdb_id(str(folder)): folder
+                for folder in sftp_client.listdir(str(jellyfin_base / "compressed" / "movie"))
+                if get_imdb_id(str(folder)) is not None
+            }
+            raw_dirs = {
+                get_imdb_id(str(folder)): folder
+                for folder in sftp_client.listdir(str(jellyfin_base / "raw" / "movie"))
+                if get_imdb_id(str(folder)) is not None
+            }
 
-    return [str(Path(dir).stem) for dir in raw_dirs - compressed_dirs]
+    missing_ids = set(raw_dirs.keys()) - set(compressed_dirs.keys())
+
+    return [str(Path(dir).name) for imdb_id, dir in raw_dirs.items() if imdb_id in missing_ids]

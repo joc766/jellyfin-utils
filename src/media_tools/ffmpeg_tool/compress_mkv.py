0ba4fdfbddc3dd3e40b86a3924cd97c370dc9499
Jack O'Connor <jackeamon@icloud.com>
2026-05-22 12:41:07 -0400

    Use configuration in compress_mkv and refactor
    Refactor logic into client + add interactive selection of files to
    compress + update tests

--- src/media_tools/ffmpeg_tool/compress_mkv.py
+++ src/media_tools/ffmpeg_tool/compress_mkv.py
@@ -1,68 +1,13 @@
-import os
-from pathlib import Path
-
-from rich.console import Console
-
 from .client import FFmpegClient
 from .progress import FFmpegProgressTracker
-
-STORAGE_BASE = Path(os.getenv("STORAGE_BASE", "/Volumes/SanDisk"))
-COMPRESSED_STORAGE_BASE = STORAGE_BASE / "compressed"
-
-console = Console(stderr=True)
-
-
-def make_output_path(
-    input_path: Path,
-    output_container: str,
-    content_type: str,
-    output_dir: Path | None = None,
-    output_filename: str | None = None,
-) -> Path:
-    if output_filename is None:
-        output_filename = input_path.stem
-    else:
-        output_filename = "".join(output_filename.split(".")[0:-1])
-
-    if output_dir is None:
-        output_dir = COMPRESSED_STORAGE_BASE / content_type / input_path.parent.stem
-
-    output_path = output_dir / f"{output_filename}.{output_container}"
-
-    return output_path
 
 
 # TODO: add dry-run parameter for easier testing and planning
 def compress_mkv(
-    input_path: Path,
-    source_type: str = "DVD",
-    content_type: str = "movie",
-    output: Path | None = None,
-    output_dir: Path | None = None,
-    output_filename: str | None = None,
-    output_container: str = "mp4",
+    client: FFmpegClient,
     overwrite: bool = False,
     verbose: bool = False,
 ):
-    if output is None:
-        output = make_output_path(
-            input_path,
-            output_container,
-            content_type=content_type,
-            output_dir=output_dir,
-            output_filename=output_filename,
-        )
-
-    if output.exists() and not overwrite:
-        raise FileExistsError(f"'{output}' already exists and overwrite=False")
-    if not output.parent.exists():
-        output.parent.mkdir(parents=True)
-
-    try:
-        client = FFmpegClient(input_path, source_type)
-    except Exception as e:
-        raise e
-
     ffprobe_info = client.get_ffprobe_info()
 
     progress = FFmpegProgressTracker(ffprobe_info["duration"])
@@ -74,12 +19,15 @@ def compress_mkv(
         else:
             deinterlace = False
         for line in client.start_compress_mkv(
-            output, overwrite=overwrite, deinterlace=deinterlace, verbose=verbose
+            overwrite=overwrite, deinterlace=deinterlace, verbose=verbose
         ):
             progress.handle_line(line)
     except InterruptedError as e:
         progress.stop_progress()
-        console.print(e, style="bold blue")
+        if client.console is not None:
+            client.console.print(e, style="bold blue")
+        else:
+            raise e
     finally:
         if not progress.stopped:
             progress.stop_progress()

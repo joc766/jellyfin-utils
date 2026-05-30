dbfb03d0805d25c05f452627da06878e30c26c1a
Jack O'Connor <jackeamon@icloud.com>
2026-05-30 10:02:48 -0400

    Refactor FFmpegProgress to be indpendent of renderer

--- src/media_tools/ffmpeg_tool/compress_mkv.py
+++ src/media_tools/ffmpeg_tool/compress_mkv.py
@@ -1,5 +1,5 @@
 from .client import FFmpegClient
-from .progress import FFmpegProgressTracker
+from .progress import FFmpegProgressRender, FFmpegProgressTracker
 
 
 # TODO: add dry-run parameter for easier testing and planning
@@ -9,19 +9,13 @@ def compress_mkv(
     verbose: bool = False,
 ):
     ffprobe_info = client.get_ffprobe_info()
-
-    progress = FFmpegProgressTracker(ffprobe_info["duration"])
-    print("Compressing with FFmpeg...")
-    try:
-        # not the most sensible to send the field_order back to the client
-        if ffprobe_info["field_order"] != "progressive":
-            deinterlace = True
-        else:
-            deinterlace = False
+    duration = ffprobe_info["duration"]
+    field_order = ffprobe_info["field_order"]
+    progress = FFmpegProgressTracker()
+    deinterlace = field_order != "progressive"
+    with FFmpegProgressRender(duration) as render:
         for line in client.start_compress_mkv(
             overwrite=overwrite, deinterlace=deinterlace, verbose=verbose
         ):
-            progress.handle_line(line)
-    finally:
-        if not progress.stopped:
-            progress.stop_progress()
+            curr_state = progress.handle_line(line)
+            render.update(curr_state)
--- src/media_tools/ffmpeg_tool/progress.py
+++ src/media_tools/ffmpeg_tool/progress.py
@@ -1,5 +1,8 @@
 import re
+from collections.abc import Iterable
+from dataclasses import dataclass
 
+from rich.console import Console
 from rich.progress import (
     BarColumn,
     Progress,
@@ -12,6 +15,13 @@ from rich.progress import (
 from rich.text import Text
 
 
+@dataclass
+class FFmpegProgressState:
+    out_time: int
+    out_time_seconds: float
+    speed: str
+
+
 class SpeedColumn(ProgressColumn):
     """Renders the upload speed from FFmpeg"""
 
@@ -20,50 +30,87 @@ class SpeedColumn(ProgressColumn):
         return Text(f"{speed}", style="green")
 
 
-class FFmpegProgressTracker:
-    out_time_pattern = re.compile("^out_time_us=(\\d+)$")
-    speed_pattern = re.compile("^speed=(\\d+\\.\\d+x)$")
+class FFmpegProgress(Progress):
+    def get_renderables(self):
+        for task in self.tasks:
+            if task.visible:
+                yield Text.from_markup(f"[bold]{task.description}")
 
-    def __init__(self, title_duration: float):
+        yield self.make_tasks_table(self.tasks)
+
+
+class FFmpegProgressRender:
+    def __init__(self, duration: float, console: Console | None = None):
         """duration: duration in seconds"""
-        self.started = False
-        self.stopped = False
-        self.duration = title_duration
-        self.task_id = None
-
-        self.progress = Progress(
-            TextColumn("[bold]{task.description}"),
-            BarColumn(),
+        self.console = console or Console()
+        self.duration = duration
+        bar_width = self.console.width if self.console.width < 80 else 80
+        self.progress = FFmpegProgress(
+            BarColumn(bar_width=bar_width),
             TextColumn("{task.percentage:5.1f}%"),
             TimeElapsedColumn(),
             TimeRemainingColumn(),
             SpeedColumn(),
+            transient=True,
         )
-
-    def start_progress(self):
-        self.started = True
-        self.progress.start()
         self.task_id = self.progress.add_task(
-            "[green]Running ffmpeg compression", total=self.duration
+            "Running ffmpeg compression", total=self.duration, start=False, visible=False
         )
 
-    def stop_progress(self):
-        self.stopped = True
-        self.progress.stop()
+    def __enter__(self):
+        self.progress.start()
+        return self
 
-    # TODO: add a track_progress function to handle full input?
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.progress.stop()
+        if exc_type is None:
+            self.console.print("FFmpeg Complete.")
+        return False
+
+    @property
+    def task(self):
+        return self.progress._tasks[self.task_id]
+
+    def update(self, new_state: FFmpegProgressState):
+        if not self.task.started:
+            self.progress.start_task(self.task_id)
+            self.progress.update(
+                self.task_id,
+                visible=True,
+                completed=new_state.out_time_seconds,
+                speed=new_state.speed,
+            )
+        else:
+            self.progress.update(
+                self.task_id, completed=new_state.out_time_seconds, speed=new_state.speed
+            )
+
+
+class FFmpegProgressTracker:
+    out_time_pattern = re.compile("^out_time_us=(\\d+)$")
+    speed_pattern = re.compile("^speed=(\\d+\\.\\d+x)$")
+
+    def __init__(self):
+        self.started = False
+        self.stopped = False
+        self.state = FFmpegProgressState(out_time=0, out_time_seconds=0, speed="")
+
+    def track_progress(self, proc: Iterable[str], verbose: bool = False):
+        for line in proc:
+            if verbose:
+                print(line)
+            yield self.handle_line(line)
 
     def handle_line(self, line: str):
-        if not self.started:
-            self.start_progress()
         line = line.rstrip("\n")
-
-        assert self.task_id is not None
 
         if out_time_match := self.out_time_pattern.match(line):
             out_time = int(out_time_match.group(1))
             out_time_seconds = out_time / 1_000_000
-            self.progress.update(self.task_id, completed=out_time_seconds)
+            self.state.out_time = out_time
+            self.state.out_time_seconds = out_time_seconds
         elif speed_match := self.speed_pattern.match(line):
             speed = speed_match.group(1)
-            self.progress.update(self.task_id, speed=speed)
+            self.state.speed = speed
+
+        return self.state

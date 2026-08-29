bf967fd827ed93d57ac7faddfa0bea8d595b80e7
Jack <razor-hautes01@icloud.com>
2026-08-29 19:32:32 -0400

    sync individual files always for consistency and accurate progress

--- src/media_tools/cli/sync.py
+++ src/media_tools/cli/sync.py
@@ -13,9 +13,6 @@ from media_tools.rsync_tool.progress import RsyncProgressTracker
 from media_tools.rsync_tool.render import RsyncRender
 
 
-# TODO: fix rsync progress when there's an existing
-# - Idea: use --include to explicitly list the selected files, that way we get better progress
-#   - Option: just sync the individual files instead of the whole folder, unless the whole folder is new
 @click.command("upload")
 @click.option("--movie", "content_type", flag_value="movie", default=True, type=str)
 @click.option("--tv", "content_type", flag_value="tv", type=str)
@@ -77,42 +74,49 @@ def upload(
 
         match content_type:
             case "movie":
-                selected = CheckboxPrompt(
-                    message="Select titles to sync",
+                selected_folder = ListPrompt(
+                    message="Select titles to sync:",
                     choices=list(new_files.keys()),
-                    instruction="Use space to select, enter to confirm.",
+                    instruction="Press Enter to select.",
                     vi_mode=True,
                 ).execute()
-                for folder_name in selected:
-                    progress = RsyncProgressTracker(
-                        title_name=folder_name, direction=client.direction
-                    )
+
+                selected_files = CheckboxPrompt(
+                    message="Select files to sync:",
+                    choices=list(new_files[selected_folder].keys()),
+                    instruction="Use Space to select, enter to confirm",
+                    vi_mode=True,
+                ).execute()
+
+                for file in selected_files:
+                    rel_file_path = Path(selected_folder) / file
+                    progress = RsyncProgressTracker(title_name=file, direction=client.direction)
                     if not silent:
                         with RsyncRender(
-                            title_name=folder_name,
+                            title_name=file,
                             direction=client.direction,
                             console=client.console,
                         ) as render:
                             for curr_state in progress.track(
-                                client.sync_subdir(folder_name, debug=debug, dry_run=dry_run),
+                                client.sync_file(rel_file_path, debug=debug, dry_run=dry_run),
                                 verbose=verbose,
                             ):
                                 render.update(curr_state)
                     else:
-                        client.sync_subdir(folder_name, debug=debug, dry_run=dry_run)
+                        client.sync_file(rel_file_path, debug=debug, dry_run=dry_run)
 
             case "tv":
                 selected_show = ListPrompt(
                     message="Select show to sync:",
                     choices=list(new_files.keys()),
-                    instruction="Use space to select, enter to confirm",
+                    instruction="Press Enter to select.",
                     vi_mode=True,
                 ).execute()
                 show_info = new_files[selected_show]
                 selected_episodes = CheckboxPrompt(
                     message="Select episodes to sync:",
                     choices=list(show_info.keys()),
-                    instruction="Use space to select, enter to confirm",
+                    instruction="Use Space to select, enter to confirm",
                     vi_mode=True,
                 ).execute()
 
@@ -207,42 +211,50 @@ def download(
 
         match content_type:
             case "movie":
-                selected = CheckboxPrompt(
+                selected_folder = ListPrompt(
                     message="Select movies to sync:",
                     choices=list(new_files.keys()),
-                    instruction="Use space to select, enter to confirm.",
+                    instruction="Press Enter to select.",
                     vi_mode=True,
                 ).execute()
-                for title_path in selected:
-                    progress = RsyncProgressTracker(
-                        title_name=title_path, direction=client.direction
-                    )
+                selected_files = CheckboxPrompt(
+                    message="Select files to sync:",
+                    choices=list(new_files[selected_folder].keys()),
+                    instruction="Use Space to select, enter to confirm.",
+                    vi_mode=True,
+                ).execute()
+
+                for file in selected_files:
+                    rel_file_path = Path(selected_folder) / file
+                    progress = RsyncProgressTracker(title_name=file, direction=client.direction)
                     if not silent:
                         with RsyncRender(
-                            title_name=title_path,
+                            title_name=file,
                             direction=client.direction,
                             console=client.console,
                         ) as render:
                             for curr_state in progress.track(
-                                client.sync_subdir(subdir=title_path, debug=debug, dry_run=dry_run),
+                                client.sync_file(
+                                    rel_file_path=rel_file_path, debug=debug, dry_run=dry_run
+                                ),
                                 verbose=verbose,
                             ):
                                 render.update(curr_state)
                     else:
-                        client.sync_subdir(subdir=title_path, debug=debug, dry_run=dry_run)
+                        client.sync_file(rel_file_path=rel_file_path, debug=debug, dry_run=dry_run)
 
             case "tv":
                 selected_show = ListPrompt(
                     message="Select show to sync:",
                     choices=list(new_files.keys()),
-                    instruction="Use space to select, enter to confirm",
+                    instruction="Press Enter to select.",
                     vi_mode=True,
                 ).execute()
                 show_info = new_files[selected_show]
                 selected_episodes = CheckboxPrompt(
                     message="Select episodes to sync:",
                     choices=list(show_info.keys()),
-                    instruction="Use space to select, enter to confirm",
+                    instruction="Use Space to select, enter to confirm",
                     vi_mode=True,
                 ).execute()
 

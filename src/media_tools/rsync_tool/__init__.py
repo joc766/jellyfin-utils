db50f5c30671f6643c8dd2f71677dc697567c0e5
Jack <razor-hautes01@icloud.com>
2026-08-29 16:51:56 -0400

    remove sync_with_server.py

--- src/media_tools/rsync_tool/__init__.py
+++ src/media_tools/rsync_tool/__init__.py
@@ -4,14 +4,11 @@ from .client import RsyncClient
 from .models import ContentFormat, ContentType, TransferDirection
 from .progress import RsyncProgressTracker
 from .render import RsyncRender
-from .sync_with_server import interactive_sync, sync_with_server
 
 __all__ = [
     "RsyncClient",
     "RsyncProgressTracker",
     "RsyncRender",
-    "interactive_sync",
-    "sync_with_server",
     "ContentType",
     "ContentFormat",
     "TransferDirection",
